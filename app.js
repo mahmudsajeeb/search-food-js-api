21cd8fd90955c5c3017832860f53ff52a44c14c5
Mahmud sajib <mahmudsajib082@gmail.com>
2023-02-24 11:38:16 +0600

    complete project

--- app.js
+++ app.js
@@ -23,6 +23,10 @@ const displayMeals = meals =>{
               <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
                 <p class="card-text">${meal.strInstructions}</p>
+                <!-- Button trigger modal ---------- -->
+            <button onclick="mealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealdetailsModal">
+              Details
+            </button>
               </div>
           </div>
     `
@@ -35,6 +39,23 @@ const buttonMeals = () =>{
   const searchField = document.getElementById("searchField").value;
   eatMeal(searchField)
 }
+
+const mealDetail = idMeal =>{
+  const url  =`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+  fetch(url)
+  .then(res =>res.json())
+  .then(data => disPlayDetailsMeals(data.meals[0]))
+}
+//dissplay Meal details function 
+const disPlayDetailsMeals = meal =>{
+  document.getElementById("mealdetailsModalLabel").innerText=meal.strMeal;
+  const mealBody = document.getElementById("mealdetailsModalBody")
+  mealBody.innerHTML = `
+    <img class="img-fluid" src=${meal.strMealThumb} />
+  `
+}
+
+//eat meal function decleration
 eatMeal('rice')
 
 
